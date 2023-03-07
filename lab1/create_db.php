4a56e2cea9493717cc31841aea9206f9fedcda0c
MishaGontar <turictscol40@gmail.com>
2023-03-07 02:20:36 +0200

    123123

--- lab1/create_db.php
+++ lab1/create_db.php
@@ -3,7 +3,7 @@ include "db_utils.php";
 
 $link = mysqli_connect("localhost", "root", "");
 if (!$link) return;
-echo "З'єднання з сервером встановлено <br>";
+echo "З'єднання з се123123123рвером встановлено <br>";
 
 $db = "MySiteDB";
 $query = "CREATE DATABASE $db";
